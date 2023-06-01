e94bc6d47ffd8ff974d15a3ca0b02134b25b7f08
Werner <guilherme.werner@tribufu.com>
2023-06-01 19:28:03 -0300

    Update reg.h

--- src/ubin/mips/reg.h
+++ src/ubin/mips/reg.h
@@ -40,6 +40,15 @@ int mips_get_reg(int reg);
 void mips_set_reg(int reg, int val);
 
 /**
+ * @brief Define o valor do registrador especificado (unsigned).
+ *
+ * @param reg O registrador a ser definido.
+ * @param val O valor a ser atribuído ao registrador.
+ * @return O valor atribuído ao registrador.
+ */
+void mips_set_reg_unsigned(int reg, unsigned int val);
+
+/**
  * @brief Retorna o nome do registrador especificado.
  *
  * @param reg O registrador para o qual o nome é desejado.
